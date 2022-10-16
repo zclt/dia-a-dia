a041987e96a4f07c856cebfe51fac5f5e4130ac5
matheus <matheus@local>
2022-10-16 16:17:23 -0300

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import { useState } from "react";
-import axios from "axios";
-import './App.css';
+import React from "react"
+import { useAuth0 } from "@auth0/auth0-react"
+import { useState } from "react"
+import axios from "axios"
+import './App.css'
 
 function App() {
-  const { loginWithRedirect, logout, user, isAuthenticated, getAccessTokenSilently } = useAuth0();
-  const [token, setToken] = useState("");
-  const [movimentacao, setMovimentacao] = useState();
-  const r1 = window.location.origin
+  const { loginWithRedirect, logout, user, isAuthenticated, getAccessTokenSilently } = useAuth0()
+  const [token, setToken] = useState()
+  const [movimentacao, setMovimentacao] = useState()
+  const logoutUri = process.env.REACT_APP_REDIRECT_URI
 
   async function asyncGetToken() {
-    setToken(isAuthenticated ? await getAccessTokenSilently({audience: process.env.REACT_APP_AUDIENCE }) : 'not logged')
-    console.log(token)
+    if(isAuthenticated && !token) {
+      const t = await getAccessTokenSilently({audience: process.env.REACT_APP_AUDIENCE })
+      setToken(t)
+      return t    
+    } else {
+      loginWithRedirect()
+    }
+    return token    
   }
 
-  function getMovimentacao(id) {
-    axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*'
-    axios.get(`/movimentacao/${id}`, {
-      headers:{
-        authorization: `Bearer ${token}`
-      }
-    }).then((response) => {
-      setMovimentacao(response.data)
-    });
+  async function asyncMovimentacao(id) {    
+    const movimentacao = await axios.get(`/movimentacao/${id}`, { headers: { authorization: `Bearer ${await asyncGetToken()}` } })
+    setMovimentacao(movimentacao.data)
   }
 
   return (
     <div className="App">
       <button onClick={() => loginWithRedirect()}>Log In</button>
-      <button onClick={() => logout({ returnTo: r1 })}>Log out</button>
-      <button onClick={() => asyncGetToken()}>Token</button>
-      <button onClick={() => getMovimentacao("62f105ff6f552f142a627040")}>Movimentacao</button>
+      <button onClick={() => logout({ returnTo: logoutUri })}>Log out</button>
+      <button onClick={() => asyncMovimentacao("62f105ff6f552f142a627040")}>Movimentacao</button>
       <h1>{ isAuthenticated ? user?.name : 'no logged' }</h1>
+      <pre>{ JSON.stringify(movimentacao, null, 4) }</pre>
     </div>
   );
 }
